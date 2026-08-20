83d6f61d2a84f3531b11077705d6523b7a89b766
Hariharan <hari85791@gmail.com>
2026-08-20 23:41:51 +0530

    Config change and report

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ CONFIG = {
     "pilot_spacing": 20,
     "f_s": 10e3,
     "f_c": 2e9,
-    "model": 'zx', # 'jk' for Jake's method, 'zx' for Zheng-Xiao's method
+    "model": 'jk', # 'jk' for Jake's method, 'zx' for Zheng-Xiao's method
     "speeds": [20, 60, 120],
-    "Eb_N0_dB_range": np.arange(0, 21, 2),
+    "Eb_N0_dB_range": np.arange(0, 26, 2),
     "M_zx": 12,    # only for Zheng-Xiao
     "model_name" : {"jk":"Smith's Model", "zx": "Zheng-Xiao's Model"}
 }
