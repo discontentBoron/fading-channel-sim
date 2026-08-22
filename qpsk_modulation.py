8b3a556b03aad7ebf950f07b1e19919c1623592a
Hariharan <hari85791@gmail.com>
2026-08-22 14:44:44 +0530

    Removed MMSE demodulation

--- qpsk_modulation.py
+++ qpsk_modulation.py
@@ -37,17 +37,3 @@ def demodulate_qpsk_zf(received_sig, h_t):
     detected_bits = interleaved_bits.flatten()
 
     return detected_bits
-
-def demodulate_qpsk_mmse(received_sig, h_t, N0):
-    h_conj = np.conj(h_t)
-    mmse_s = h_conj/(np.abs(h_t)**2 + N0)
-    eq_sig = mmse_s * received_sig
-    detected_sig_real = eq_sig.real
-    detected_sig_imag = eq_sig.imag
-
-    detected_b1 = (detected_sig_real > 0).astype(int)
-    detected_b0 = (detected_sig_imag > 0).astype(int)
-
-    interleaved_bits = np.column_stack((detected_b1, detected_b0))
-    detected_bits = interleaved_bits.flatten()
-    return detected_bits
